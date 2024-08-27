c4600f402bd5b1c1f79ee4cf766a979ae78f7ce4
Neeraj Verma <neerajverma101@gmail.com>
2024-08-27 23:12:06 +0530

    issues fixes

--- constant/index.tsx
+++ constant/index.tsx
@@ -8,7 +8,6 @@ export const APIS = {
     LIVE_CLASS: "https://elearning-class.app.100ms.live/meeting/:roomCode",
     SESSIONS_BY_ROOM_ID: "/sessions?roomId=:roomId&active=true",
     SESSION_BY_SESSION_ID: "/sessions/:sessionId",
-    SCHEMA_BY_SERVICE: '/api/schema/:service',
     CREATE_ORGANIZATION: "/api/organization/create",
     USER_LOGIN: "/api/auth/login",
 
