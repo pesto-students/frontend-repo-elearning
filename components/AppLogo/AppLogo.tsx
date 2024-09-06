ab7ed902ea46bd87abe58548223068b6164b34c4
vinayak-pk <vinayak.pk97@gmail.com>
2024-09-06 18:10:48 +0530

    chore: updated applogo

--- components/AppLogo/AppLogo.tsx
+++ components/AppLogo/AppLogo.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Logo from "../../app/public/elearning-logo.svg";
 
-const AppLogo = () => {
+const AppLogo = ({path}: {path: string}) => {
     return (
-        <Link href='/'>
+        <Link href={path}>
             <Image src={Logo} alt='Logo' style={{ objectFit: 'contain' }} width={150} ></Image>
         </Link>
     );
